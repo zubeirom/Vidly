32f862767e736fa9e942467ac9182ef1abdec389
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-07-01 19:44:02 +0200

    Work with actionresult

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,22 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
